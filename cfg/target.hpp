990693d72f3bd316c5d3385afebb39a8bb7c4b98
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-03-10 18:20:11 -0300

    🔧 Add topic and name to target.hpp

--- cfg/target.hpp
+++ cfg/target.hpp
@@ -1,15 +1,17 @@
 #ifndef __TARGET_HPP__
 #define __TARGET_HPP__
 
-#include <cstdint>
-
 #include "proxy/button.hpp"
 #include "proxy/led.hpp"
 
-proxy::Led::Config led_config = {
+proxy::Led::Config led_config{
+    "led_node", // name
+    "led",      // topic
 };
 
-proxy::Button::Config button_config = {
+proxy::Button::Config button_config{
+    "button_node", // name
+    "button",      // topic
 };
 
 #endif // __TARGET_HPP__
