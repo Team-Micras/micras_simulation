b5555817ad8cdbf7a8046d3e642c649c7a4ba4ef
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-10 13:01:01 -0300

    ✨ Add target configuration files

---
+++ cfg/target.hpp
@@ -0,0 +1,15 @@
+#ifndef __TARGET_HPP__
+#define __TARGET_HPP__
+
+#include <cstdint>
+
+#include "proxy/button.hpp"
+#include "proxy/led.hpp"
+
+proxy::Led::Config led_config = {
+};
+
+proxy::Button::Config button_config = {
+};
+
+#endif // __TARGET_HPP__
