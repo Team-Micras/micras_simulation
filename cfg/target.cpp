8828b69f602528d4e805149e42f26738187ad5ee
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-03-19 02:11:38 -0300

    🔧 Add proxies configuration to target

--- cfg/target.cpp
+++ cfg/target.cpp
@@ -10,12 +10,64 @@
 
 std::shared_ptr<rclcpp::Node> micras_node;
 
+proxy::Button::Config button_config{
+    micras_node, // node
+    "button",    // topic
+};
+
+proxy::DistanceSensor::Config distance_sensor_0_config{
+    micras_node,         // node
+    "distance_sensor_0", // topic
+};
+
+proxy::DistanceSensor::Config distance_sensor_1_config{
+    micras_node,         // node
+    "distance_sensor_1", // topic
+};
+
+proxy::DistanceSensor::Config distance_sensor_2_config{
+    micras_node,         // node
+    "distance_sensor_2", // topic
+};
+
+proxy::DistanceSensor::Config distance_sensor_3_config{
+    micras_node,         // node
+    "distance_sensor_3", // topic
+};
+
+proxy::Encoder::Config encoder_left_config{
+    micras_node,    // node
+    "encoder_left", // topic
+};
+
+proxy::Encoder::Config encoder_right_config{
+    micras_node,     // node
+    "encoder_right", // topic
+};
+
+proxy::Imu::Config imu_config{
+    micras_node, // node
+    "imu",       // topic
+};
+
 proxy::Led::Config led_config{
     micras_node, // node
     "led",       // topic
 };
 
-proxy::Button::Config button_config{
+proxy::Locomotion::Config locomotion_config{
     micras_node, // node
-    "button",    // topic
+    "cmd_vel",   // topic
+};
+
+proxy::TorqueSensor::Config torque_sensor_left_config{
+    micras_node,  // node
+    "torque_flw", // front_topic
+    "torque_rlw", // rear_topic
+};
+
+proxy::TorqueSensor::Config torque_sensor_right_config{
+    micras_node,  // node
+    "torque_frw", // front_topic
+    "torque_rrw", // rear_topic
 };
--- cfg/target.hpp
+++ cfg/target.hpp
@@ -13,11 +13,26 @@
 #include <rclcpp/rclcpp.hpp>
 
 #include "proxy/button.hpp"
+#include "proxy/distance_sensor.hpp"
+#include "proxy/encoder.hpp"
+#include "proxy/imu.hpp"
 #include "proxy/led.hpp"
+#include "proxy/locomotion.hpp"
+#include "proxy/torque_sensor.hpp"
 
 extern std::shared_ptr<rclcpp::Node> micras_node;
 
-extern proxy::Led::Config led_config;
 extern proxy::Button::Config button_config;
+extern proxy::DistanceSensor::Config distance_sensor_0_config;
+extern proxy::DistanceSensor::Config distance_sensor_1_config;
+extern proxy::DistanceSensor::Config distance_sensor_2_config;
+extern proxy::DistanceSensor::Config distance_sensor_3_config;
+extern proxy::Encoder::Config encoder_left_config;
+extern proxy::Encoder::Config encoder_right_config;
+extern proxy::Imu::Config imu_config;
+extern proxy::Led::Config led_config;
+extern proxy::Locomotion::Config locomotion_config;
+extern proxy::TorqueSensor::Config torque_sensor_left_config;
+extern proxy::TorqueSensor::Config torque_sensor_right_config;
 
 #endif // __TARGET_HPP__
