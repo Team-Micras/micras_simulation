add4280cddf134966372acf987a8b7ae70a0d591
PedroDeSanti <phmartinsanti@gmail.com>
2024-03-27 06:30:18 -0300

    ✨ Add button status enum and implement button press detection

--- include/micras_simulation/proxy/button.hpp
+++ include/micras_simulation/proxy/button.hpp
@@ -20,28 +20,66 @@ namespace proxy {
 class Button {
     public:
         /**
+         * @brief Enum for button status
+         */
+        enum Status {
+            NO_PRESS,
+            SHORT_PRESS,
+            LONG_PRESS,
+            EXTRA_LONG_PRESS
+        };
+
+        /**
          * @brief Configuration structure for button
          */
         struct Config {
             std::shared_ptr<rclcpp::Node>& node;
             std::string                    topic;
+            uint16_t                       long_press_delay = 1000;
+            uint16_t                       extra_long_press_delay = 5000;
         };
 
         /**
-         * @brief Constructor for the Button class
+         * @brief Constructor for Button class
          *
-         * @param button_config Configuration for the button
+         * @param config Button configuration
          */
-        Button(const Config& button_config);
+        Button(Config& config);
 
         /**
-         * @brief Get the button state
+         * @brief Check if button is pressed
          *
-         * @return true if the button is pressed, false otherwise
+         * @return True if button is pressed, false otherwise
          */
-        bool get_state() const;
+        bool is_pressed();
+
+        /**
+         * @brief Get button status
+         *
+         * @return Button status
+         */
+        Status get_status();
 
     private:
+        /**
+         * @brief Update button state
+         */
+        void update_state();
+
+        /**
+         * @brief Check if button was just pressed
+         *
+         * @return True if button was just pressed, false otherwise
+         */
+        bool is_rising_edge() const;
+
+        /**
+         * @brief Check if button was just released
+         *
+         * @return True if button was just released, false otherwise
+         */
+        bool is_falling_edge() const;
+
         /**
          * @brief Current state of the button
          */
@@ -51,6 +89,27 @@ class Button {
          * @brief Subscriber for the button state topic
          */
         rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr subscriber;
+
+        /**
+         * @brief Button pressing delays in ms
+         */
+        const uint16_t long_press_delay;
+        const uint16_t extra_long_press_delay;
+
+        /**
+         * @brief Timer for button press time
+         */
+        std::chrono::time_point<std::chrono::steady_clock> press_time;
+
+        /**
+         * @brief Flag to know if button was being pressed
+         */
+        bool previous_state = false;
+
+        /**
+         * @brief Flag to know if button is being pressed
+         */
+        bool current_state = false;
 };
 }  // namespace proxy
 
