1fc332f286518c81840de799b740112a5e2dc362
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-05 02:18:00 -0300

    ✨ Add proxy Battery class implementation and declaration

---
+++ include/micras_simulation/proxy/battery.hpp
@@ -0,0 +1,65 @@
+/**
+ * @file battery.hpp
+ *
+ * @brief Proxy Battery class declaration
+ *
+ * @date 04/2024
+ */
+
+#ifndef MICRAS_PROXY_BATTERY_HPP
+#define MICRAS_PROXY_BATTERY_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/u_int32.hpp>
+#include <string>
+#include <cstdint>
+
+namespace proxy {
+/**
+ * @brief Class for getting the battery voltage
+ */
+class Battery {
+    public:
+        /**
+         * @brief Configuration structure for the battery
+         */
+        struct Config {
+            std::shared_ptr<rclcpp::Node>& node;
+            std::string                    topic;
+        };
+
+        /**
+         * @brief Constructor for the Battery class
+         *
+         * @param config Configuration for the battery
+         */
+        explicit Battery(const Config& config);
+
+        /**
+         * @brief Get the battery voltage
+         *
+         * @return float Battery voltage in volts
+         */
+        float get_voltage() const;
+
+        /**
+         * @brief Get the raw reading from the battery
+         *
+         * @return uint32_t Raw reading from the battery
+         */
+        uint32_t get_voltage_raw() const;
+
+    private:
+        /**
+         * @brief Reading from the battery
+         */
+        uint32_t reading{ };
+
+        /**
+         * @brief Subscriber for the battery topic
+         */
+        rclcpp::Subscription<std_msgs::msg::UInt32>::SharedPtr subscriber;
+};
+}  // namespace proxy
+
+#endif // MICRAS_PROXY_BATTERY_HPP
---
+++ src/lib/battery.cpp
@@ -0,0 +1,26 @@
+/**
+ * @file battery.cpp
+ *
+ * @brief Proxy Battery class implementation
+ *
+ * @date 04/2024
+ */
+
+#include "proxy/battery.hpp"
+
+namespace proxy {
+Battery::Battery(const Config& config) {
+    this->subscriber = config.node->create_subscription<std_msgs::msg::UInt32>(
+        config.topic, 1, [this](const std_msgs::msg::UInt32& msg) {
+            this->reading = msg.data;
+        });
+}
+
+float Battery::get_voltage() const {
+    return this->reading * 8.4 / 4095;
+}
+
+uint32_t Battery::get_voltage_raw() const {
+    return this->reading;
+}
+}  // namespace proxy
