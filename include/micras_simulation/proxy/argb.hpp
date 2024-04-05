2e205e2555de7729eacf352c99864eaa26cac71c
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-05 00:37:38 -0300

    ✨ Add Argb class for controlling addressable RGB LEDs

---
+++ include/micras_simulation/proxy/argb.hpp
@@ -0,0 +1,86 @@
+/**
+ * @file argb.hpp
+ *
+ * @brief Proxy Argb class declaration
+ *
+ * @date 03/2024
+ */
+
+#ifndef MICRAS_PROXY_ARGB_HPP
+#define MICRAS_PROXY_ARGB_HPP
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/color_rgba.hpp>
+#include <string>
+#include <array>
+#include <cstdint>
+
+namespace proxy {
+/**
+ * @brief Class for controlling an addressable RGB LED
+ */
+template <uint8_t num_of_leds>
+class Argb {
+    public:
+        /**
+         * @brief Configuration structure for the addressable RGB LED
+         */
+        struct Config {
+            std::shared_ptr<rclcpp::Node>&       node;
+            std::array<std::string, num_of_leds> topic_array;
+        };
+
+        /**
+         * @brief Structure for storing color information
+         */
+        struct Color {
+            uint8_t red;
+            uint8_t green;
+            uint8_t blue;
+        };
+
+        /**
+         * @brief Constructor for the Argb class
+         *
+         * @param config Configuration for the addressable RGB LED
+         */
+        explicit Argb(const Config& config);
+
+        /**
+         * @brief Set the color of the ARGB at the specified index
+         *
+         * @param index The index of the ARGB to set the color of
+         * @param color The color to set the ARGB to
+         */
+        void set_color(const Color& color, uint8_t index);
+
+        /**
+         * @brief Set the color of all ARGBs
+         *
+         * @param color The color to set all ARGBs to
+         */
+        void set_color(const Color& color);
+
+        /**
+         * @brief Turn off the ARGB at the specified index
+         *
+         * @param index The index of the ARGB to turn off
+         */
+        void turn_off(uint8_t index);
+
+        /**
+         * @brief Turn off all ARGBs
+         */
+        void turn_off();
+
+    private:
+        /**
+         * @brief Array of publishers for the ARGBs
+         */
+        std::array<std::shared_ptr<rclcpp::Publisher<std_msgs::msg::ColorRGBA>>, num_of_leds> publishers;
+};
+}  // namespace proxy
+
+#include "../src/proxy/argb.cpp"  // NOLINT(bugprone-suspicious-include)
+
+#endif // MICRAS_PROXY_ARGB_HPP
---
+++ src/lib/argb.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file argb.cpp
+ *
+ * @brief Proxy Argb class implementation
+ *
+ * @date 03/2024
+ */
+
+#ifndef MICRAS_PROXY_ARGB_CPP
+#define MICRAS_PROXY_ARGB_CPP
+
+#include "proxy/argb.hpp"
+
+namespace proxy {
+template <uint8_t num_of_leds>
+Argb<num_of_leds>::Argb(const Config& config) {
+    for (uint8_t i = 0; i < num_of_leds; i++) {
+        this->publishers.at(i) = config.node->create_publisher<std_msgs::msg::ColorRGBA>(config.topic_array.at(i), 1);
+    }
+}
+
+template <uint8_t num_of_leds>
+void Argb<num_of_leds>::set_color(const Color& color, uint8_t index) {
+    if (index >= num_of_leds) {
+        return;
+    }
+
+    this->publishers.at(index)->publish({color.red, color.green, color.blue, 1});
+}
+
+template <uint8_t num_of_leds>
+void Argb<num_of_leds>::set_color(const Color& color) {
+    for (uint8_t i = 0; i < num_of_leds; i++) {
+        this->publishers.at(i)->publish({color.red, color.green, color.blue, 1});
+    }
+}
+
+template <uint8_t num_of_leds>
+void Argb<num_of_leds>::turn_off(uint8_t index) {
+    this->set_color(index, {0, 0, 0});
+}
+
+template <uint8_t num_of_leds>
+void Argb<num_of_leds>::turn_off() {
+    this->set_color({0, 0, 0});
+}
+}  // namespace proxy
+
+#endif // MICRAS_PROXY_ARGB_CPP
