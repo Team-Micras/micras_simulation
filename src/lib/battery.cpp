b009b31a992697c624a12c98a0c4393a1b40496c
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-09 06:43:58 -0300

    🔨 Update battery.cpp to use float32 message type for battery subscriber

--- src/lib/battery.cpp
+++ src/lib/battery.cpp
@@ -10,16 +10,16 @@
 
 namespace micras::proxy {
 Battery::Battery(const Config& config) {
-    this->subscriber = config.node->create_subscription<std_msgs::msg::UInt32>(
-        config.topic, 1, [this](const std_msgs::msg::UInt32& msg) { this->reading = msg.data; }
+    this->subscriber = config.node->create_subscription<std_msgs::msg::Float32>(
+        config.topic, 1, [this](const std_msgs::msg::Float32& msg) { this->reading = msg.data; }
     );
 }
 
 float Battery::get_voltage() const {
-    return this->reading * 8.4 / 4095;
+    return this->reading;
 }
 
 uint32_t Battery::get_voltage_raw() const {
-    return this->reading;
+    return ((this->reading) / 8.4) * 4095;
 }
 }  // namespace micras::proxy
