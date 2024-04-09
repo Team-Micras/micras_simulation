86bc09278212396226b1d87d21e6452a3b26e48e
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-09 00:43:55 -0300

    🚧 ✨ Add Micras plugin to the gazebo/micras_plugin directory

---
+++ gazebo/micras_plugin/MicrasPlugin.cc
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <ignition/plugin/Register.hh>
+
+#include "MicrasPlugin.hh"
+
+using namespace ignition;
+using namespace gui;
+
+/////////////////////////////////////////////////
+MicrasPlugin::MicrasPlugin() : Plugin() { }
+
+/////////////////////////////////////////////////
+MicrasPlugin::~MicrasPlugin() { }
+
+/////////////////////////////////////////////////
+void MicrasPlugin::LoadConfig(const tinyxml2::XMLElement* _pluginElem) {
+    if (!_pluginElem)
+        return;
+
+    // Take parameters from XML at runtime
+    auto messageElem = _pluginElem->FirstChildElement("message");
+    if (nullptr != messageElem && nullptr != messageElem->GetText())
+        this->message = messageElem->GetText();
+}
+
+/////////////////////////////////////////////////
+void MicrasPlugin::OnButton() {
+    std::cout << this->message << std::endl;
+}
+
+// Register this plugin
+IGNITION_ADD_PLUGIN(ignition::gui::MicrasPlugin, ignition::gui::Plugin);
---
+++ gazebo/micras_plugin/MicrasPlugin.hh
@@ -0,0 +1,59 @@
+/*
+ * Copyright (C) 2017 Open Source Robotics Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef IGNITION_GUI_HELLOPLUGIN_HH_
+#define IGNITION_GUI_HELLOPLUGIN_HH_
+
+#include <string>
+
+#include <ignition/gui/qt.h>
+#include <ignition/gui/Plugin.hh>
+
+namespace ignition {
+namespace gui {
+class MicrasPlugin : public Plugin {
+    Q_OBJECT
+
+    /// \brief Constructor
+
+public:
+    MicrasPlugin();
+
+    /// \brief Destructor
+
+public:
+    virtual ~MicrasPlugin();
+
+    /// \brief Called by Ignition GUI when plugin is instantiated.
+    /// \param[in] _pluginElem XML configuration for this plugin.
+
+public:
+    virtual void LoadConfig(const tinyxml2::XMLElement* _pluginElem) override;
+
+    /// \brief Callback trigged when the button is pressed.
+protected slots:
+    void OnButton();
+
+    /// \brief Message to be printed when button is pressed.
+
+private:
+    std::string message{"Micras, plugin!"};
+};
+}  // namespace gui
+}  // namespace ignition
+
+#endif
