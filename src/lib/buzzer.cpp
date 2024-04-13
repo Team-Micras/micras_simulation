102cbeaf046817357af998cedf7933ed047357de
GabrielCosme <cosmebarbosa.gabriel@gmail.com>
2024-04-13 12:32:42 -0300

    🐛 Fix buzzer wait function

--- src/lib/buzzer.cpp
+++ src/lib/buzzer.cpp
@@ -19,24 +19,24 @@ void Buzzer::play(uint32_t frequency, uint32_t duration) {
     this->message.data = frequency;
     this->publisher->publish(message);
     this->is_playing = true;
+    this->duration = duration;
 
     if (duration > 0) {
-        this->duration = duration;
         this->playing_timer.reset_ms();
     }
 }
 
 void Buzzer::update() {
-    if (not this->is_playing) {
-        return;
-    }
-
-    if (this->duration > 0 and this->playing_timer.elapsed_time_ms() > this->duration) {
+    if (this->is_playing and this->duration > 0 and this->playing_timer.elapsed_time_ms() > this->duration) {
         this->stop();
     }
 }
 
 void Buzzer::wait(uint32_t duration) {
+    while (this->duration > 0 and this->is_playing) {
+        this->update();
+    }
+
     hal::Timer wait_timer;
 
     while (wait_timer.elapsed_time_ms() < duration) {
