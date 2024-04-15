73190e02cf0cac698cb6545c916ac9faf7d313b8
PedroDeSanti <phmartinsanti@gmail.com>
2024-04-15 00:06:53 -0300

    ⚡️ Update micras_node.cpp to use MultiThreadedExecutor

--- src/bin/micras_node.cpp
+++ src/bin/micras_node.cpp
@@ -17,11 +17,19 @@ int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
 
     micras::micras_node = std::make_shared<rclcpp::Node>("micras_node");
+    rclcpp::Node::SharedPtr main_node = std::make_shared<rclcpp::Node>("main_node");
+
+    rclcpp::executors::MultiThreadedExecutor executor;
+
     micras::MicrasController micras_controller;
 
     try {
-        auto timer = micras::micras_node->create_wall_timer(10ms, [&micras_controller]() { micras_controller.run(); });
-        rclcpp::spin(micras::micras_node);
+        auto timer = main_node->create_wall_timer(10ms, [&micras_controller]() { micras_controller.run(); });
+
+        executor.add_node(main_node);
+        executor.add_node(micras::micras_node);
+        executor.spin();
+
     } catch (const std::exception& e) {
         RCLCPP_ERROR_STREAM(micras::micras_node->get_logger(), e.what());
     }
